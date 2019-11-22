0685a1b25fe079774bf49a1cea3b798b10078e08
javigomez <javier@typeform.com>
2019-11-22 15:00:48 +0100

    feat: full screen widget

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import styled from 'styled-components'
 
-function App() {
+const Widget = styled.li`
+background: #e2dfdd;
+margin: 10px;
+width: 25vw;
+height: 25vh;
+list-style: none;
+border-radius: 8px;
+border: 2px solid #a4a3a3;
+`
+const FullScreenWidget = styled.div`
+background: #e2dfdd;
+margin: 60px;
+list-style: none;
+border-radius: 8px;
+border: 2px solid #a4a3a3;
+position: absolute;
+top: 0;
+bottom: 0;
+left: 0;
+right: 0;
+display: flex;
+align-items: center;
+justify-content: center;
+`
+
+const Wrapper = styled.ul`
+  display: flex;
+`
+
+
+function App(props) {
+  console.log(props)
+  const [selectedWidget, setSelectedWidget] = useState(props.initialData.selectedWidget)
+
+  const widget = props.initialData.widgets.find(widget => widget.id === selectedWidget)
+
+  const widgetList = props.initialData.widgets.map(widget => {
+    function handleClick () {
+      setSelectedWidget(widget.id)
+    }
+
+    return (<Widget key={widget.id} onClick={handleClick}>{JSON.stringify(widget.data)}</Widget>)
+  } ) 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrapper>
+        {widgetList}
+      </Wrapper>
+      {widget &&  <FullScreenWidget>{JSON.stringify(widget.data)}</FullScreenWidget>}
     </div>
   );
 }
